fffc8988c2350bec9bf01fea5868fff8bd989b45
Tom Ainsworth <tomainsworth94@gmail.com>
2022-06-07 15:48:03 +0100

    Create transcription model

--- transcriptions/admin.py
+++ transcriptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transcription
 
-# Register your models here.
+
+@admin.register(Transcription)
+class TranscriptionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("song_name", "artist_name")}
+    list_display = ("song_name", "artist_name", "author")
+    search_fields = ["song_name", "artist_name", "author"]
--- transcriptions/models.py
+++ transcriptions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Transcription(models.Model):
+    song_name = models.CharField(max_length=30)
+    artist_name = models.CharField(max_length=30)
+    featured_image = CloudinaryField("image")
+    chart = CloudinaryField("file")
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="transcriber"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["song_name", "artist_name"]
+
+    def __str__(self):
+        return f"{self.song_name} by {self.artist_name}"
