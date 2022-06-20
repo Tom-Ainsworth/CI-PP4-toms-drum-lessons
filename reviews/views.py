0fae7ed6a55966a7c1c65574dfff8b4261ba6c0a
Tom Ainsworth <tomainsworth94@gmail.com>
2022-06-20 18:21:19 +0100

    Fix: add safe filter to review.body to remoove html tags

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from home.views import PageTitleViewMixin
 from .forms import ReviewForm
 from .models import Review
 
 
 class ReviewList(PageTitleViewMixin, generic.ListView):
+    """List view for displaying user reviews"""
+
     title = "Reviews"
     template_name = "reviews.html"
+    context_object_name = "review_list"
 
-    model = Review
     queryset = Review.objects.filter(approved=True).order_by("created_on")
     paginate_by = 3
