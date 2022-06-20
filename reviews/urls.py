b9fce612d75224cd4f6c3730cb543f44640b6742
Tom Ainsworth <tomainsworth94@gmail.com>
2022-06-20 23:57:57 +0100

    Feat: Add CreateReview functionality

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewList.as_view(), name="reviews"),
+    path("create_review", views.CreateReview.as_view(), name="create_review"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -14,3 +14,35 @@ class ReviewList(PageTitleViewMixin, generic.ListView):
 
     queryset = Review.objects.filter(approved=True).order_by("created_on")
     paginate_by = 3
+
+
+class CreateReview(PageTitleViewMixin, View):
+    """Class View to add a new review"""
+
+    title = "Create Review"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "create_review.html",
+            {"review_form": ReviewForm()},
+        )
+
+    def post(self, request, *args, **kwargs):
+        """post the new review to the database"""
+
+        review_form = ReviewForm(request.POST)
+
+        if review_form.is_valid():
+            review_form.instance.author_id = request.user.id
+            review_form.save()
+        else:
+            review_form = ReviewForm()
+
+        return render(
+            request,
+            "reviews.html",
+            {
+                "reviewed": True,
+            },
+        )
