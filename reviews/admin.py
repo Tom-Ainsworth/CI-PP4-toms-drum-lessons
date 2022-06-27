58e0b72ac03d98fa2e3f73c813ab55a5fa98e65e
Tom Ainsworth <tomainsworth94@gmail.com>
2022-06-27 19:38:00 +0100

    Add readme images. Modify reviews model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from .models import Review
-from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Review)
-class ReviewAdmin(SummernoteModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
 
     list_display = ("created_on", "author", "approved")
     search_fields = ["title", "author"]
-    summernote_fields = "body"
     actions = ["approve_review"]
 
     def approve_review(self, request, queryset):
--- reviews/models.py
+++ reviews/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Review(models.Model):
     title = models.CharField(max_length=30)
-    body = models.TextField(max_length=300)
+    body = models.TextField(max_length=1000)
     author = models.ForeignKey(User, related_name="reviewer", on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
